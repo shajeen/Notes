6dbd6e42fee35326feca48b728f51e116e58c3ba
shajeen ahamed <shajeenahmed@gmail.com>
2021-01-20 12:20:44 +0530

    |

--- tutorialspoint/CSharp/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-using namespace System;
-int main()
-{
-    System::Console::WriteLine("This is a C++/CLI program.");
-}
--- tutorialspoint/CSharp/main_example.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace test {
-    class test {
-        public static void Main(string[] args) {
-            System.Console.WriteLine("C# from cmake!.");
-        }
-    }
-};
